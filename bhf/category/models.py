97ffa219e1e8f55e2c6364922d3a7bb2c1dd2aff
atifar <tyatya68@gmail.com>
2015-09-28 16:35:03 -0700

    Add partially complete models

--- bhf/category/models.py
+++ bhf/category/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from inventory.models import Image
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    keyword_list = models.TextField(max_length=1500, blank=True)
+    image = models.ManyToManyField(Image)
+
+    def __str__(self):
+        return self.name
+
+
+class Subcategory(models.Model):
+    category_id = models.ForeignKey(Category)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    keyword_list = models.TextField(max_length=1500, blank=True)
+    image = models.ManyToManyField(Image)
+
+    def __str__(self):
+        return self.name
+
+
--- bhf/inventory/models.py
+++ bhf/inventory/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    short_description = models.TextField(max_length=250, blank=True)
+    special_order = models.BooleanField()
