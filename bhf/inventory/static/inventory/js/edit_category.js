60b5f41fc5be3cdccaa362cacbeda35204193308
atifar <tyatya68@gmail.com>
2015-11-28 18:33:21 -0800

    Improve render button behavior.

--- bhf/inventory/static/inventory/js/edit_category.js
+++ bhf/inventory/static/inventory/js/edit_category.js
@@ -1,20 +1,19 @@
 // Handles the rendering of text area field content that may be HTML
-// formatted text.
+// formatted text. Clicking the render button renders the contents
+// from the corresponding text area input field.
 
 // ///////////// Category description /////////////
 // Grab the render button, description textarea, output text box
-var render_button = document.getElementById("render");
+var render_button = document.getElementById('render');
 
 // Set up a listener to render button click
-render_button.addEventListener('click', function (e) {
+render_button.addEventListener('click', function(e) {
     renderDescription(e);
 });
 
 function renderDescription(e) {
     // Grab the content of the description's 'textarea' element
-    var description = $("#id_description").html();
-    // Decode the encoded HTML content
-    var decoded = $("<div/>").html(description).text();
-    // Insert the decoded HTML into the render box
-    $('#output_box').html(decoded);
+    var description = $('#id_description').val();
+    // Insert the description into the render box
+    $('#output_box').html(description);
 };
